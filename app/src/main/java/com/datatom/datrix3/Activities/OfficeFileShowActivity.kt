12426de6f37fa7e500d12aa71b8310157943892b
wgz <wang.guangzu@datatom.com>
2018-05-08 17:41:27 +0800

    1741

--- app/src/main/java/com/datatom/datrix3/Activities/OfficeFileShowActivity.kt
+++ app/src/main/java/com/datatom/datrix3/Activities/OfficeFileShowActivity.kt
@@ -2,6 +2,7 @@ package com.datatom.datrix3.Activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.view.View
 import com.datatom.datrix3.R
 import com.datatom.datrix3.BaseActivity
@@ -20,14 +21,23 @@ import org.jetbrains.anko.toast
 import java.lang.Exception
 import java.util.concurrent.TimeUnit
 
+import java.io.File.separator
+import android.os.Environment.getExternalStorageDirectory
+import android.text.TextUtils
+import android.widget.RelativeLayout
+import com.tencent.smtt.sdk.TbsReaderView
+import java.io.File
+
+
 /**
  * office文件下载预览
  */
-class OfficeFileShowActivity : BaseActivity() {
+class OfficeFileShowActivity : BaseActivity() ,TbsReaderView.ReaderCallback{
+    override fun onCallBackAction(p0: Int?, p1: Any?, p2: Any?) {
 
+    }
 
-
-
+    private var mTbsReaderView: TbsReaderView? = null
     private lateinit var database: AppDatabase
 
     override fun ActivityLayout(): Int {
@@ -36,9 +46,10 @@ class OfficeFileShowActivity : BaseActivity() {
     }
 
     override fun initView() {
-
+        mTbsReaderView = TbsReaderView(this, this)
         var data = intent.getSerializableExtra("file")
         database = AppDatabase.getInstance(this)
+        rl_file.addView(mTbsReaderView, RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT))
 
         when(data){
             is PersonalFilelistData.result2 ->{
@@ -82,6 +93,46 @@ class OfficeFileShowActivity : BaseActivity() {
 
     }
 
+    private fun openFile(path: String) :Boolean {
+        val tempPath = File("${Environment.getExternalStorageDirectory()}/officetemp")
+        if (!tempPath.exists())
+            tempPath.mkdirs()
+        //通过bundle把文件传给x5,打开的事情交由x5处理
+        val bundle = Bundle()
+        //传递文件路径
+        bundle.putString("filePath", File(path).toString())
+        //加载插件保存的路径
+        bundle.putString("tempPath",  "${Environment.getExternalStorageDirectory()}/officetemp")
+        if (this.mTbsReaderView == null)
+            this.mTbsReaderView = TbsReaderView(this, this)
+        //加载文件前的初始化工作,加载支持不同格式的插件
+        val b = mTbsReaderView!!.preOpen(getFileType(File(path).toString()), false)
+        if (b) {
+            mTbsReaderView!!.openFile(bundle)
+        }
+
+        return b
+
+
+    }
+
+
+
+    private fun getFileType(path: String): String {
+        var str = ""
+
+        if (TextUtils.isEmpty(path)) {
+            return str
+        }
+        val i = path.lastIndexOf('.')
+        if (i <= -1) {
+            return str
+        }
+        str = path.substring(i + 1)
+        return str
+    }
+
+
     private fun openfile(data: TaskFile) {
 
         try {
@@ -89,9 +140,11 @@ class OfficeFileShowActivity : BaseActivity() {
 
                 "docx" -> {
                     this!!.startActivity(IntentUtil.getWordFileIntent(data.filePath))
+
                 }
                 "doc" -> {
                     this!!.startActivity(IntentUtil.getWordFileIntent(data.filePath))
+
                 }
                 "xls" -> {
                     this!!.startActivity(IntentUtil.getExcelFileIntent(data.filePath))
@@ -127,61 +180,187 @@ class OfficeFileShowActivity : BaseActivity() {
         var url = HttpUtil.BASEAPI_URL + "/datrix3/viewer/dcomp.php?fileidstr=" + data.fileid + "&iswindows=0&optuser=admin"
 
         url.LogD(" 下载URL ： ")
+        val futureStudioIconFile = File(Environment.getExternalStorageDirectory().toString() + File.separator
+                + "datrixdownload")
+        if (!futureStudioIconFile.exists())
+            futureStudioIconFile.mkdirs()
+        val file = File(futureStudioIconFile,  data.filename)
+        if (file.exists()){
+            when (data.ext) {
 
-        val download  = HttpUtil.instance.downLoadApi(officeFile).downloadFileWithFixedUrl(url)
-                .subscribeOn(Schedulers.io())
-                .observeOn(Schedulers.newThread())
-                .subscribe({
-
-                    var result = Someutil.writeResponseBodyToDisk2(it, data.filename)
-                    var file = result.second
-
-                    if (result.first) {
-                        "下载成功".LogD()
-                        when (data.ext) {
-
-                            "docx" -> {
-                                this!!.startActivity(IntentUtil.getWordFileIntent(file.absolutePath))
-                            }
-                            "doc" -> {
-                                this!!.startActivity(IntentUtil.getWordFileIntent(file.absolutePath))
-                            }
-                            "xls" -> {
-                                this!!.startActivity(IntentUtil.getExcelFileIntent(file.absolutePath))
-                            }
-                            "xlsx" -> {
-                                this!!.startActivity(IntentUtil.getExcelFileIntent(file.absolutePath))
-                            }
-                            "ppt" -> {
-                                this!!.startActivity(IntentUtil.textopen(file.absolutePath, "ppt"))
-                            }
-                            "pptx" -> {
-                                this!!.startActivity(IntentUtil.getPptFileIntent(file.absolutePath))
-                            }
-                            "txt" -> {
-                                this!!.startActivity(IntentUtil.getTextFileIntent(file.absolutePath, false))
-                            }
-                            else -> "未找到类型"
-
+                "docx" -> {
+                    //
+                    if (!openFile(file.absolutePath)){
+                        this!!.startActivity(IntentUtil.getWordFileIntent(file.absolutePath))
+                        runOnUiThread {
+                                downfile_name.text = "没有可打开的应用！"
+                                file_downloadProgress.visibility = View.INVISIBLE
                         }
-
-                    } else {
-                        "下载失败".LogD()
-
                     }
-                }, {
-                    it.toString().LogD("download error : ")
-                    runOnUiThread {
-                        if (it.toString().contains("No Activity found")) {
-                            downfile_name.text = "没有找到打开此文件的应用！"
+                }
+                "doc" -> {
+                    //
+                    if (!openFile(file.absolutePath)){
+                        this!!.startActivity(IntentUtil.getWordFileIntent(file.absolutePath))
+                        runOnUiThread {
+                            downfile_name.text = "没有可打开的应用！"
                             file_downloadProgress.visibility = View.INVISIBLE
+                        }
+                    }
+                }
+                "xls" -> {
+                    // this!!.startActivity(IntentUtil.getExcelFileIntent(file.absolutePath))
+                    if (!openFile(file.absolutePath)){
+                        this!!.startActivity(IntentUtil.getWordFileIntent(file.absolutePath))
+                        runOnUiThread {
+                            downfile_name.text = "没有可打开的应用！"
+                            file_downloadProgress.visibility = View.INVISIBLE
+                        }
+                    }
+                }
+                "xlsx" -> {
+                    if (!openFile(file.absolutePath)){
+                        this!!.startActivity(IntentUtil.getExcelFileIntent(file.absolutePath))
+                        runOnUiThread {
+                            downfile_name.text = "没有可打开的应用！"
+                            file_downloadProgress.visibility = View.INVISIBLE
+                        }
+                    }
+                }
+                "ppt" -> {
+                    if (!openFile(file.absolutePath)){
+                        this!!.startActivity(IntentUtil.textopen(file.absolutePath, "ppt"))
+                        runOnUiThread {
+                            downfile_name.text = "没有可打开的应用！"
+                            file_downloadProgress.visibility = View.INVISIBLE
+                        }
+                    }
+                }
+                "pptx" -> {
+                    if (!openFile(file.absolutePath)){
+                        this!!.startActivity(IntentUtil.getPptFileIntent(file.absolutePath))
+                        runOnUiThread {
+                            downfile_name.text = "没有可打开的应用！"
+                            file_downloadProgress.visibility = View.INVISIBLE
+                        }
+                    }
+                }
+                "txt" -> {
+                    if (!openFile(file.absolutePath)){
+                        this!!.startActivity(IntentUtil.getTextFileIntent(file.absolutePath, false))
+                        runOnUiThread {
+                            downfile_name.text = "没有可打开的应用！"
+                            file_downloadProgress.visibility = View.INVISIBLE
+                        }
+                    }
+                }
+                else -> "未找到类型"
+
+            }
+        }else{
+            val download  = HttpUtil.instance.downLoadApi(officeFile).downloadFileWithFixedUrl(url)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(Schedulers.newThread())
+                    .subscribe({
+
+                        var result = Someutil.writeResponseBodyToDisk2(it, data.filename)
+                        var file = result.second
+
+                        if (result.first) {
+                            "下载成功".LogD()
+                            when (data.ext) {
+
+                                "docx" -> {
+                                    //
+                                    if (!openFile(file.absolutePath)){
+                                        this!!.startActivity(IntentUtil.getWordFileIntent(file.absolutePath))
+                                        runOnUiThread {
+                                            downfile_name.text = "没有可打开的应用！"
+                                            file_downloadProgress.visibility = View.INVISIBLE
+                                        }
+                                    }
+                                }
+                                "doc" -> {
+                                    //
+                                    if (!openFile(file.absolutePath)){
+                                        this!!.startActivity(IntentUtil.getWordFileIntent(file.absolutePath))
+                                        runOnUiThread {
+                                            downfile_name.text = "没有可打开的应用！"
+                                            file_downloadProgress.visibility = View.INVISIBLE
+                                        }
+                                    }
+                                }
+                                "xls" -> {
+                                    // this!!.startActivity(IntentUtil.getExcelFileIntent(file.absolutePath))
+                                    if (!openFile(file.absolutePath)){
+                                        this!!.startActivity(IntentUtil.getWordFileIntent(file.absolutePath))
+                                        runOnUiThread {
+                                            downfile_name.text = "没有可打开的应用！"
+                                            file_downloadProgress.visibility = View.INVISIBLE
+                                        }
+                                    }
+                                }
+                                "xlsx" -> {
+                                    if (!openFile(file.absolutePath)){
+                                        this!!.startActivity(IntentUtil.getExcelFileIntent(file.absolutePath))
+                                        runOnUiThread {
+                                            downfile_name.text = "没有可打开的应用！"
+                                            file_downloadProgress.visibility = View.INVISIBLE
+                                        }
+                                    }
+                                }
+                                "ppt" -> {
+                                    if (!openFile(file.absolutePath)){
+                                        this!!.startActivity(IntentUtil.textopen(file.absolutePath, "ppt"))
+                                        runOnUiThread {
+                                            downfile_name.text = "没有可打开的应用！"
+                                            file_downloadProgress.visibility = View.INVISIBLE
+                                        }
+                                    }
+                                }
+                                "pptx" -> {
+                                    if (!openFile(file.absolutePath)){
+                                        this!!.startActivity(IntentUtil.getPptFileIntent(file.absolutePath))
+                                        runOnUiThread {
+                                            downfile_name.text = "没有可打开的应用！"
+                                            file_downloadProgress.visibility = View.INVISIBLE
+                                        }
+                                    }
+                                }
+                                "txt" -> {
+                                    if (!openFile(file.absolutePath)){
+                                        this!!.startActivity(IntentUtil.getTextFileIntent(file.absolutePath, false))
+                                        runOnUiThread {
+                                            downfile_name.text = "没有可打开的应用！"
+                                            file_downloadProgress.visibility = View.INVISIBLE
+                                        }
+                                    }
+                                }
+                                else -> "未找到类型"
+
+                            }
+
+                        } else {
+                            "下载失败".LogD()
+
+                        }
+                    }, {
+                        it.toString().LogD("download error : ")
+                        runOnUiThread {
+                            if (it.toString().contains("No Activity found")) {
+                                downfile_name.text = "没有找到打开此文件的应用！"
+                                file_downloadProgress.visibility = View.INVISIBLE
+
+                            }
 
                         }
 
-                    }
+                    })
 
-                })
+            addsub(download)
+        }
 
-        addsub(download)
+
+
     }
 }
--- app/src/main/java/com/datatom/datrix3/app.kt
+++ app/src/main/java/com/datatom/datrix3/app.kt
@@ -4,6 +4,11 @@ import android.app.Application
 import android.content.Context
 import com.datatom.datrix3.Util.HttpUtil
 import com.danikula.videocache.HttpProxyCacheServer
+import com.datatom.datrix3.helpers.LogD
+import com.tencent.smtt.sdk.QbSdk
+import com.tencent.smtt.sdk.TbsListener
+
+
 
 
 /**
@@ -22,6 +27,40 @@ class app : Application() {
         mapp = this
        // proxy = newProxy()
         HttpUtil.instance.setContext(applicationContext)
+        QbSdk.setDownloadWithoutWifi(true)
+
+        QbSdk.initX5Environment(this, object : QbSdk.PreInitCallback {
+            override fun onCoreInitFinished() {
+               "".LogD("QbSdk onCoreInitFinished::::  ")
+            }
+
+            override fun onViewInitFinished(b: Boolean) {
+
+                b.toString().LogD("QbSdk onViewInitFinished::::  ")
+                //这里被回调，并且b=true说明内核初始化并可以使用
+                //如果b=false,内核会尝试安装，你可以通过下面监听接口获知
+            }
+        })
+
+        QbSdk.setTbsListener(object : TbsListener {
+            override fun onDownloadFinish(i: Int) {
+                //tbs内核下载完成回调
+                i.toString().LogD("QbSdk onDownloadFinish : ")
+            }
+
+            override fun onInstallFinish(i: Int) {
+                i.toString().LogD("QbSdk onInstallFinish : ")
+                //内核安装完成回调，
+            }
+
+            override fun onDownloadProgress(i: Int) {
+                i.toString().LogD("QbSdk onDownloadProgress : ")
+                //下载进度监听
+            }
+        })
+
+
+
 
     }
 
